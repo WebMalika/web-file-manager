14489086bfb2d37eabf0454d9a063f3fc02f7acf
lolipopmari <mit.maria2019@yandex.ru>
2022-04-28 00:49:52 +0300

    реализована навигация по папкам и новая структура хранения данных

--- main.js
+++ main.js
@@ -1,67 +1,91 @@
+//TODO
+if(!localStorage.getItem('folders')){
+    localStorage.setItem('folders', JSON.stringify([{
+        parent: null,
+        id: 0,
+        name: "/",
+        isPrivate: false,
+        dateCreate: '2022-02-12'
+    }]))
+}
+
+if(!localStorage.getItem('files') || localStorage.getItem('files') == 'false'){
+    localStorage.setItem('files', JSON.stringify([[]]))
+}
+
 const app = Vue.createApp({
     data(){
         return {
-            disk: [
-                {
-                    name: 'gfgrf',
-                    type: 'dir',
-                    isPrivate: false,
-                    disk: []
-                },
-                {
-                    name: 'index.php',
-                    type: 'file'
-                }
-            ],
-            curDisk: [
-                {
-                    name: 'gfgrf',
-                    type: 'dir',
-                    isPrivate: false,
-                    disk: [
-                        {
-                            name: 'auywdgtagdsgdiua',
-                            type: 'dir',
-                            isPrivate: false,
-                            disk: []
-                        },
-                        {
-                            name: 'asKALOP',
-                            type: 'dir',
-                            isPrivate: true,
-                            disk: []
-                        },
-                        {
-                            name: 'SDAS.php',
-                            type: 'file'
-                        }
-                    ]
-                },
-                {
-                    name: 'index.php',
-                    type: 'file'
-                }
-            ],
+            // main data
+            folders: JSON.parse(localStorage.getItem('folders')),
+            files: JSON.parse(localStorage.getItem('files')),
+
+            // navigation 
+            curFolderID: 0,
+            curDisk: null,
+            counterFolders: 0,
+            prevFolder: false,
+            nextFolder: false,
+
+            // modal for add object
             isVisiblePrivateModal: false,
             isVisibleAddModal: false,
             typeAddObj: null,
+            privateStatusAddObj: false,
+            nameAddObj: '',
+            textAddModal: '',
+
+            // other
             visiblePath: '/',
-            originalPath: null,
-            textAddModal: ''
         }
     },
+    mounted: function () { 
+        this.curDisk = this.folders.filter(item => item.parent == this.curFolderID);
+        this.counterFolders = this.folders.length;
+    },
     methods: {
-        toFolder(key) {
-            console.log(this.curDisk);
-            if(!this.curDisk[key].isPrivate && this.curDisk[key].disk){
-                this.visiblePath += this.curDisk[key].name + '/'
-                this.curDisk = this.curDisk[key].disk
+        toFolder(folder) {
+            if(!folder.isPrivate){
+                this.prevFolder = this.curFolderID;
+                this.nextFolder = false;
 
-            } else {
-
+                this.visiblePath += folder.name + '/';
+                this.curFolderID = folder.id;
             }
-            console.log(this.curDisk);
         },
+
+        toPrevFolder(){
+            if(this.prevFolder === false) return;
+
+            this.nextFolder = this.curFolderID;
+            this.curFolderID = this.prevFolder;
+            
+            let parFolder = this.folders.find(item => item.id == this.curFolderID);
+            if(parFolder.parent != null)
+                this.prevFolder = parFolder.parent;
+            else 
+                this.prevFolder = false;
+
+            // TODO 
+            let tmpPath = this.visiblePath.split("/");
+            tmpPath.pop();
+            tmpPath.pop();
+            this.visiblePath = tmpPath.join('/') + '/';
+        },
+
+        toNextFolder(){
+            if(this.nextFolder === false) return;
+            console.log(this.nextFolder);
+
+            this.prevFolder = this.curFolderID;
+            this.curFolderID = this.nextFolder;
+
+            let parFolder = this.folders.find(item => item.id == this.curFolderID);
+            this.visiblePath += parFolder.name + '/';
+
+            this.nextFolder = false;
+        },
+
         visibleModal(typeAddObj){
             this.typeAddObj = typeAddObj;
             
@@ -74,33 +98,52 @@ const app = Vue.createApp({
                 this.isVisibleAddModal = true;
             }
         },
+
         addNewObj(){
-            let name = this.$refs.objName.value,
-                isPrivate = this.$refs.objPrivate.checked ? true : false;
-            
-            if(!name){
-                this.$refs.modal__error.innerHTML = `<span class='fatal__error'><b>Ошибка!</b> Вы не ввели названия!</span>`
+            if(!this.nameAddObj){
+                this.$refs.modal__error.innerHTML = `<span class='fatal__error'><b>Ошибка!</b> Вы не ввели названиe!</span>`
                 this.$refs.objName.classList.add('field_error');
                 return;
             }
             
-            if(this.typeAddObj == 'dir')
-                this.curDisk.push({
-                    name: name,
-                    type: this.typeAddObj,
-                    isPrivate: isPrivate,
-                    disk: []
-                });
-            else 
-                this.curDisk.push({
-                    name: name,
-                    type: this.typeAddObj,
-                    isPrivate: isPrivate
-                });
+            let today = new Date(),
+                dd = String(today.getDate()).padStart(2, '0'),
+                mm = String(today.getMonth() + 1).padStart(2, '0'),
+                yyyy = today.getFullYear();
+            
+            if(this.typeAddObj == 'dir'){
+                this.folders.push({
+                    parent: this.curFolderID,
+                    id: this.counterFolders,
+                    name: this.nameAddObj,
+                    isPrivate: this.privateStatusAddObj,
+                    dateCreate: yyyy + '-' + mm + '-' + dd
+                })
+                this.counterFolders++;
+
+                this.files.push([]);
+            } else {
+                this.files[this.curFolderID].push({
+                    name: this.nameAddObj,
+                    isPrivate: this.privateStatusAddObj,
+                    dateCreate: yyyy + '-' + mm + '-' + dd
+                })
+            }
 
             this.isVisibleAddModal = false;
             this.$refs.modal__error.innerHTML = '';
             this.$refs.objName.classList.remove('field_error');
+            this.nameAddObj = ''
+        }
+    },
+    computed: {
+        updateCurDisk: function() {
+            if(!this.folders) return;
+            
+            localStorage.setItem('folders', JSON.stringify(this.folders));
+            localStorage.setItem('files', JSON.stringify(this.files));
+
+            return this.curDisk = this.folders.filter(item => item.parent == this.curFolderID);
         }
     }
 })
